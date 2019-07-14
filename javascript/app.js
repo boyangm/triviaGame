1e2f23d28cb80c7763500aa2c7b8cb79db065de8
Boyang Matsapola <boyangbass@gmail.com>
2019-07-13 21:23:41 -0500

    fixed bugs

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 window.onload = function (){
     var questions = [];
-    var count = 45;
+    var count = 5;
     var num = 0;
     var marker= 0;
     var right= 0;
@@ -18,24 +18,27 @@ window.onload = function (){
     }
     function clock(){
         $("#timer").html("<h4>"+ count +"</h4><br><p>Seconds left</p>" );
+        count --;
         // console.log(count);
         if( count < 45 && Number.isInteger(count/15) ){
             $(".notAnswer").eq(marker).css("opacity", 0);
             marker++;
         } 
         if( count === 0){
+            setTimeout(reset,1000);
+            // clearInterval(interval); 
             wrong++;
+            $("#timer").empty();
+            newDiv.empty();
+            newDiv.append("<h2>Out of time</h2>");
             console.log("wrong: "+ wrong);
-            reset();
-            alert('out of time');
         }
-        count --;
     };
 
-    var interval =setInterval(clock, 1000);
-
+    
     
     newDiv= $("<div>");
+    var interval =setInterval(clock, 1000);
     function getQuestion(){
         $(newDiv).append("<h2> Question: "+ (num +1) +" </h2>");
         $(newDiv).append("<h2>"+ questions[num].question+ "</h2>");
@@ -54,27 +57,36 @@ window.onload = function (){
         newDiv.append(correct);
         $("#questionBox").append(newDiv);
         $(".answer").on("click",()=>{
+            right++;
+            $("#timer").empty();
+            setTimeout(reset,1000);
+    
             newDiv.empty();
             newDiv.append("<h2>Congrats! that was correct!</h2>");
-            setTimeout(reset,4000);
-           reset();
-           right++;
-           console.log("right: " + right);
-        
+            console.log("right: " + right);
         })
+            
     };
     function reset (){
         num++; 
-       if( num > 9){ 
-        $("#timer").empty();
-        clearInterval(interval);
-        newDiv.empty();
-        newDiv.append("<h2>Congrats!</h2>");
-        newDiv.append("<h2>You answered "+ right + " right</h2>");
-    }else{
-        newDiv.empty(); 
-        count = 45; 
-        getQuestion();
+        if( num > 9){ 
+            $("#timer").empty();
+            clearInterval(interval); 
+            newDiv.empty();
+            newDiv.append("<h2>Congrats!</h2>");
+            newDiv.append("<h2>You answered "+ right + " right</h2>");
+        }else{
+            $("#timer").empty();
+
+            newDiv.empty();  
+            count = 45; 
+            if (marker < 2){
+                marker= num;
+            }else{
+                marker= 0;
+            }
+           
+            getQuestion();
         
         }
     }
